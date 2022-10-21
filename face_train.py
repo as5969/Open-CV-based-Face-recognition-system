d8577d7352447cc4b6dbdfaddc046131a8d6553c
as5969 <91520767+as5969@users.noreply.github.com>
2022-10-21 18:21:21 +0530

    Add files via upload

---
+++ face_train.py
@@ -0,0 +1,50 @@
+import os
+import cv2 as cv
+import numpy as np
+
+people = ['Brad Pitt', 'Dylan O Brian', 'Jennifer Anniston']
+
+DIR = r'D:\OPEN CV Face Recognition'
+Haar_Cascades = cv.CascadeClassifier('haas_cascade.xml')
+improved_cascade = cv.CascadeClassifier('improved_cascade.xml')
+left_cascade = cv.CascadeClassifier('left_eye.xml')
+haar_cascadehtt = cv.CascadeClassifier('haar_cascadehtt.xml')
+
+features=[]
+labels=[]
+def create_train():
+	for person in people:
+		path = os.path.join(DIR, person)
+		label = people.index(person)
+
+		for img in os.listdir(path):
+			img_path = os.path.join(path,img)
+
+
+			img_array = cv.imread(img_path)
+			gray = cv.cvtColor(img_array,cv.COLOR_BGR2GRAY)
+
+			faces_rect = Haar_Cascades.detectMultiScale(gray,scaleFactor =1.1 , minNeighbors=4)
+
+			for (x,y,w,h) in faces_rect:
+				faces_roi = gray[y:y+h,x:x+w]
+				features.append(faces_roi)
+				labels.append(label)
+create_train()
+print('training model done')
+features = np.array(features , dtype ='object')  
+labels = np.array(labels)
+
+
+print(f'Length of the features = {len(features)}')
+print(f'Length of the labels = {len(labels)}')
+
+face_recog = cv.face.LBPHFaceRecognizer_create()
+face_recog.train(features,labels)
+face_recog.save('face_trained.yml')
+
+np.save('features.npy',features)
+np.save('labels.npy',labels)
+
+
+
